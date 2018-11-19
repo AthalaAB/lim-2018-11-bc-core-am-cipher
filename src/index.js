e17fdacf7b8412195d94b950f5a5cca1b785fa6f
AthalaAB <athala.abanto@gmail.com>
2018-11-18 23:47:24 -0500

    Primer Avance

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function pagCifrar(){
+   document.getElementById("primeraPag").style.display = "none";
+   document.getElementById("segundaPag").style.display = "block";
+}
+function pagDescifrar(){
+   document.getElementById("primeraPag").style.display = "none";
+   document.getElementById("terceraPag").style.display = "block";
+}
+function pag1(){
+   document.getElementById("primeraPag").style.display = "block";
+   document.getElementById("segundaPag").style.display = "none";
+}
+//
+function msnACifrar(){
+ const texto= document.getElementsByName("putMessage1").value;
+ for(let i=0; i>=texto.lenght; i++){
+   console.log(text[i])
+}
+
+const textoCifrado=
+document.getElementById().innerHTML=textoCifrado
