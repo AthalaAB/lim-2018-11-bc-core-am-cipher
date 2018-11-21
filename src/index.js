e00fc3de55a77d7c5ac86c9d7d82170291aa3b60
AthalaAB <athala.abanto@gmail.com>
2018-11-21 13:24:28 -0500

    Descuartizando función descifrado

--- src/index.js
+++ src/index.js
@@ -15,46 +15,30 @@ function pag1B(){
    document.getElementById("tercera-Pag").style.display = "none";
 }
 //pasar a cipher.js
-//cifrado en consola por trabajar
-var ingreso= 2
-function cifrando (str) {
-var num = 0;
-  let newStr = "";
-  for ( let i=0; i<str.length; i++ ) {
-    num = str.charCodeAt(i);
-    if ( num >= 65) {
-      num += ingreso;
-    }
-    if ( num > 90 ) {
-      num -= 26;
-    }
-    newStr += String.fromCharCode(num);
-  }
-  return newStr;
-}
- cifrando("HOLA");
-
 
 //descifrado
-const resultadoDescifrado = document.getElementById("resultado");
-function descifrando (str , number) {
-var num = 0;
+const resultadoDescifrado = document.getElementById("resultado").value;
+function descifrando(str,number) {
+/*let num = 0;
   let newStr = "";
   for ( let i=0; i<str.length; i++ ) {
     num = str.charCodeAt(i);
     if ( num >= 65) {
       num -= number;
     }
-    if ( num > 90 ) {
+    if ( num >= 90 ) {
       num += 26;
     }
     newStr += String.fromCharCode(num);
   }
   return newStr;
+  */
+  alert (number)
 }
-vamosADescifrar.addEventListener("click", (e)=>{
-  e.preventDefault();
-  const numP = document.getElementById("put-Number1").value;
-  const ingresaMensaje = document.getElementById("ingresa-mensaje").value;
-  resultadoDescifrado.innerHTML = descifrando(ingresaMensaje , numP);
+const vamosADescifrar = document.getElementById("vamos-descifrar");
+vamosADescifrar.addEventListener("click", ()=>{
+  //e.preventDefault();
+  const numP = document.getElementById("put-Number2").value;
+  const ingresaMensaje = document.getElementById("ingresa-mensaje2").value;
+  resultadoDescifrado.innerHTML = descifrando(ingresaMensaje,numP);
 })
