12e84784e2ca0fe66f8465963f4796214f9ee5f9
AthalaAB <athala.abanto@gmail.com>
2018-11-25 14:37:57 -0500

    Uso de cipher.js, empezando corrección final de CSS>

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,30 @@
 window.cipher = {
-  // ... 
+  encode:function cifrando(ctr,number1) {
+    const upMessageC = ctr.toUpperCase();
+    let numC;
+      let newCtr = "";
+      let texto1;
+      for ( let i=0; i<upMessageC.length; i++ ) {
+        numC = upMessageC.charCodeAt(i);
+        if ( numC >= 65 && numC <= 90) {
+           texto1 = (( numC - 65 + number1 ) % 26) + 65;
+        }
+        newCtr += String.fromCharCode(texto1);
+      }
+      return newCtr;
+    },
+  decode: function descifrando(str,number) {
+    const upMessageD = str.toUpperCase();
+    let num;
+      let newStr = "";
+      let texto;
+      for ( let i=0; i<upMessageD.length; i++ ) {
+        num = upMessageD.charCodeAt(i);
+        if ( num >= 65 && num <= 90) {
+           texto = (( num - 90 - number ) % 26) + 90;
+        }
+        newStr += String.fromCharCode(texto);
+      }
+      return newStr;
+    },
 };
--- src/index.js
+++ src/index.js
@@ -17,43 +17,19 @@ function pag1B(){
 //pasar a cipher.js
 //descifrado
 const resultadoDescifrado = document.getElementById("resultado");
-function descifrando(str,number) {
-let num;
-  let newStr = "";
-  let texto;
-  for ( let i=0; i<str.length; i++ ) {
-    num = str.charCodeAt(i);
-    if ( num >= 65 && num <= 90) {
-       texto = (( num - 90 - number ) % 26) + 90;
-    }
-    newStr += String.fromCharCode(texto);
-  }
-  return newStr;
-}
+
 const vamosADescifrar = document.getElementById("vamos-descifrar");
 vamosADescifrar.addEventListener("click", ()=>{
   const numP = document.getElementById("put-Number2").value;
   const ingresaMensaje = document.getElementById("ingresa-mensaje2").value;
-  resultadoDescifrado.innerHTML = descifrando(ingresaMensaje,parseInt(numP));
+  resultadoDescifrado.innerHTML = cipher.decode(ingresaMensaje,parseInt(numP));
 })
 //cifrado
 const resultadoCifrado = document.getElementById("resultadoC");
-function cifrando(ctr,number1) {
-let numC;
-  let newCtr = "";
-  let texto1;
-  for ( let i=0; i<ctr.length; i++ ) {
-    numC = ctr.charCodeAt(i);
-    if ( numC >= 65 && numC <= 90) {
-       texto1 = (( numC - 65 + number1 ) % 26) + 65;
-    }
-    newCtr += String.fromCharCode(texto1);
-  }
-  return newCtr;
-}
+
 const vamosACifrar = document.getElementById("vamos-cifrar");
 vamosACifrar.addEventListener("click", ()=>{
   const numS = document.getElementById("put-Number1").value;
   const ingresaMensaje1 = document.getElementById("ingresa-mensaje").value;
-  resultadoCifrado.innerHTML = cifrando(ingresaMensaje1,parseInt(numS));
+  resultadoCifrado.innerHTML = cipher.encode(ingresaMensaje1,parseInt(numS));
 })
