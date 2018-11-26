fccb5e49911209436d41211c752f72e018522546
AthalaAB <athala.abanto@gmail.com>
2018-11-25 23:42:40 -0500

    Avance del CSS casi al 100%: barra de redes sociales, botones, form y responsive

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-function pagCifrar(){
+const vamosAPagCifrar= document.getElementById("pag-cifrar");
+vamosAPagCifrar.addEventListener("click", function pagCifrar(){
    document.getElementById("primera-Pag").style.display = "none";
    document.getElementById("segunda-Pag").style.display = "block";
-}
-function pagDescifrar(){
+})
+
+const vamosAPagDescifrar= document.getElementById("pag-descifrar");
+vamosAPagDescifrar.addEventListener("click", function pagDescifrar(){
    document.getElementById("primera-Pag").style.display = "none";
    document.getElementById("tercera-Pag").style.display = "block";
-}
-function pag1(){
+})
+
+const vamosAPag1A= document.getElementById("a-pag1");
+vamosAPag1A.addEventListener("click", function pag1(){
    document.getElementById("primera-Pag").style.display = "block";
    document.getElementById("segunda-Pag").style.display = "none";
-}
-function pag1B(){
+})
+
+const vamosAPag1B= document.getElementById("b-pag1");
+vamosAPag1B.addEventListener("click", function pag1B(){
    document.getElementById("primera-Pag").style.display = "block";
    document.getElementById("tercera-Pag").style.display = "none";
-}
-//pasar a cipher.js
+})
 //descifrado
 const resultadoDescifrado = document.getElementById("resultado");
 
