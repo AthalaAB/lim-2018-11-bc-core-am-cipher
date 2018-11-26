42d463d5261904aa61d7d07e1e573f53c519128c
AthalaAB <athala.abanto@gmail.com>
2018-11-26 10:00:09 -0500

    Cifrado y descifrado con minúsculas, espacios y números

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,20 @@
 window.cipher = {
   encode:function cifrando(ctr,number1) {
     const upMessageC = ctr.toUpperCase();
-    let numC;
+    let numC; /*a mayúsculas*/
       let newCtr = "";
-      let texto1;
+      let texto1; /*para aplicar la fórmula*/
       for ( let i=0; i<upMessageC.length; i++ ) {
         numC = upMessageC.charCodeAt(i);
         if ( numC >= 65 && numC <= 90) {
            texto1 = (( numC - 65 + number1 ) % 26) + 65;
+        }
+
+        if ( numC===32){
+          texto1= numC;
+        }
+        if( numC>=48 && numC<=57){
+          texto1 = numC
         }
         newCtr += String.fromCharCode(texto1);
       }
@@ -22,6 +29,12 @@ window.cipher = {
         num = upMessageD.charCodeAt(i);
         if ( num >= 65 && num <= 90) {
            texto = (( num - 90 - number ) % 26) + 90;
+        }
+        if ( num===32){
+          texto= num;
+        }
+        if( num>=48 && num<=57){
+          texto = num
         }
         newStr += String.fromCharCode(texto);
       }
