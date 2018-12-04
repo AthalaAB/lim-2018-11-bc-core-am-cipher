682a345733f8aae9470a9c8182beb1c593c034e8
AthalaAB <athala.abanto@gmail.com>
2018-12-04 09:24:37 -0500

    corrección de test: orden

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,9 +10,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () =>{  assert.equal(cipher.encode("ABCDEFGHIJKLMNOPQRSTUVWXYZ",33),"HIJKLMNOPQRSTUVWXYZABCDEFG")});
-    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () =>{assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz",33),"hijklmnopqrstuvwxyzabcdefg")});
-    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?" para " !#$%&()*+,-./0123456789:;<=>?" con offest 33', () =>{assert.equal(cipher.encode(" !#$%&()*+,-./0123456789:;<=>?",33)," !#$%&()*+,-./0123456789:;<=>?")});
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () =>{  assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG")});
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offest 33', () =>{assert.equal(cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg")});
+    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?" para " !#$%&()*+,-./0123456789:;<=>?" con offest 33', () =>{assert.equal(cipher.encode(33, " !#$%&()*+,-./0123456789:;<=>?")," !#$%&()*+,-./0123456789:;<=>?")});
   });
 
   describe('cipher.decode', () => {
@@ -21,8 +21,8 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () =>{  assert.equal(cipher.decode("HIJKLMNOPQRSTUVWXYZABCDEFG", 33),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")});
-    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () =>{  assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33),"abcdefghijklmnopqrstuvwxyz")});
-    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?" para " !#$%&()*+,-./0123456789:;<=>?" con offest 33', () =>{  assert.equal(cipher.decode(" !#$%&()*+,-./0123456789:;<=>?", 33)," !#$%&()*+,-./0123456789:;<=>?")});
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () =>{  assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")});
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33', () =>{  assert.equal(cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz")});
+    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?" para " !#$%&()*+,-./0123456789:;<=>?" con offest 33', () =>{  assert.equal(cipher.decode(33, " !#$%&()*+,-./0123456789:;<=>?")," !#$%&()*+,-./0123456789:;<=>?")});
   });
 });
